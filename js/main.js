a2b9007c2dde496f08635ab4ab17b72ee07db87c
ASQAlex <72416314+ASQAlex@users.noreply.github.com>
2022-06-01 20:35:29 +0200

    some

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
+const swiper = new Swiper('.swiper', {
+    loop: true,
 
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
 });
