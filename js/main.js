7ac69647e07de7352c75910c2818f527c55f2912
ASQAlex <72416314+ASQAlex@users.noreply.github.com>
2022-06-23 19:21:02 +0200

    some

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-const swiper = new Swiper('.swiper', {
+var swiper1 = new Swiper('.swiper1', {
     loop: true,
+    autoplay: true,
 
     pagination: {
         el: ".swiper-pagination",
@@ -21,4 +22,17 @@ const swiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev',
     },
 
+});
+
+var swiper2 = new Swiper('.swiper2', {
+    loop: true,
+    autoplay: true,
+    slidesPerView: 1.5,
+    spaceBetween: 30,
+
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 });
